dc062334bbfc5aeb867b8f8b037e152523e0084c
gastronome <1305573134@qq.com>
2024-11-23 15:30:25 +0000

    删除文件 frontend

--- frontend/src/api/sub_status.js
+++
@@ -1,5 +0,0 @@
-import request from '@/utils/request.js'
-
-export const getSubStatusService = () => request.get(`/sub-status`)
-
-export const updateSubStatusService = (data) => request.put(`/sub-status`, data)
